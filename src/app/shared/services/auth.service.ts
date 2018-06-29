cf25229c3a09470941c6b895542308216f6caa1e
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2018-06-29 17:41:39 +0200

    Added functionality to auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@Injectable()
 export class AuthService {
-  isLoggedIn() {
-    return true;
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+  signIn(email: string, password: string): Promise<any> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  signOut(): Promise<any> {
+    return this.afAuth.auth.signOut();
   }
 }
