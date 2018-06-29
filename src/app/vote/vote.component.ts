5ea49a793282615beb04b70eab4e356802bbba1e
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2018-06-29 11:53:55 +0200

    Added voting sceleton

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from 'src/app/shared/car.model';
 
 @Component({
   selector: 'app-vote',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
+  car: Car = {
+    pictures: ['https://i.pinimg.com/736x/2d/c7/72/2dc772ca80d8adb6a169a3815c46e284--matte-black-cars-matte-cars.jpg'],
+    brand: 'BMW',
+    model: '7',
+    engine: 'What ever',
+    topSpeed: 300,
+    acceleration: 3,
+    userId: '',
+    id: '',
+    description: `Lorem ipsum dolor amet leggings hella green juice mlkshk tilde scenester.
+                  Before they sold out skateboard retro, farm-to-table salvia unicorn air
+                  plant fam. Meditation fashion axe seitan tumeric raclette banjo street art.
+                  Mumblecore af cold-pressed coloring book pug chia.`,
+  };
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
