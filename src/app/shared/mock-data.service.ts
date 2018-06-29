7e1f809f6bfea654a239f00232869dded2c993a7
Natalio Sacerdote <natalio.3@gmail.com>
2018-06-29 12:31:57 +0100

    add mock data for users

--- src/app/shared/mock-data.service.ts
+++ src/app/shared/mock-data.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, Inject, InjectionToken } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Car } from 'src/app/shared/car.model';
+import {User} from './user.model';
 
 export const ENDPOINT_URL = new InjectionToken<string>('endpointUrl');
 
@@ -15,6 +16,21 @@ export class MockDataService {
     private http: HttpClient,
     @Inject(ENDPOINT_URL) private endPointUrl: string
   ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.endPointUrl}/user-data.json`);
+  }
+
+  getUsersByRankRange(fromRank = 1, toRank = 25): Observable<User[]> {
+    return this.getUsers().pipe(
+      map((users) => {
+        if (toRank > users.length) {
+          toRank = users.length;
+        }
+        return users.slice(fromRank-1, toRank);
+      })
+    );
+  }
 
   getCars(): Observable<Car[]> {
     return this.http.get<Car[]>(`${this.endPointUrl}/car-data.json`);
