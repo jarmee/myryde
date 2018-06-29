87e9f5709b95103796037f3dfb13fc12527e9954
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2018-06-29 20:24:37 +0200

    Added current user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  currentUser: any;
+  constructor(private authService: AuthService) {
+    this.currentUser = authService.currentUser;
+  }
+
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.currentUser;
+  }
+
+
+  ngAfterViewInit(): void {
+    this.currentUser = this.authService.currentUser;
+  }
 }
